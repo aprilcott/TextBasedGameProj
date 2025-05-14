f302b3f76523506761e715386a0a1a084f69a454
LeftRightGoodnite524 <sunshinesavannah23@gmail.com>
2025-05-14 17:01:42 +1000

    yo

--- game/src/characters/Player.java
+++ game/src/characters/Player.java
@@ -2,8 +2,26 @@ package characters;
 
 public class Player {
     private String damage;
-    Private Integer Playerx, PlayerY;
+    private Integer Playerx, PlayerY;
+    private String PlayerName;
+    private String PlayerDialogue;
+    private Integer PlayerHealth;
+    private Integer PlayerDamageMin;
     private String getDamage() {
         return this.damage;
+}
+public String getHealth() {
+    return this.Health;
+    private Integer CalculateDamage() {
+        return random.nextInt(this.DamageMin, this.DamageMax);
     }
+    private Integer TakeDamage() {
+        this.Health = this.Health - MC.getDamage;
+    }
+ 
+    public Player(Integer Health, String Name, String Dialogue, Integer DamageMin, Integer DamageMax) {
+        
+    }
+Player Astelle = new Player(100, "Astelle", "I won't let you hurt them", 7, 10);
+}
 }
